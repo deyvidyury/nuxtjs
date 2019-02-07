a576f2b9cfa39367f7c9bd22992c6665df9ea67d
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-02-07 16:31:53 -0300

    routing

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -1,13 +1,19 @@
 <template>
   <section class="container">
-    <h1>Hello</h1>
+    <nuxt-link to="/users">Users</nuxt-link>
+    <br>
+    <button @click="onLoadProducts">Load Products</button>
   </section>
 </template>
 
 <script>
 
 export default {
-
+  methods: {
+    onLoadProducts() {
+      this.$router.push('/products');
+    }
+  }
 }
 </script>
 
